6f8d676d199cb7ad3b2fbe6fb2931f43ea57e5e5
Connor Hewitt <connor.hewitt@gmail.com>
2022-10-07 19:57:19 -0500

    Remove react-bootstrap and friends

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,17 @@
-import { Nav, Navbar } from "react-bootstrap";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import "../styles/globals.scss";
 import store from "../store";
 
-function MyLink({ href, children }) {
-  const router = useRouter();
-  const currentPath = router.pathname;
-
-  return (
-    <Link href={href} passHref>
-      <Nav.Link active={currentPath == href}>{children}</Nav.Link>
-    </Link>
-  );
-}
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Navbar expand="lg" bg="dark" variant="dark">
+      <div>
         <Link href="/" passHref>
-          <Navbar.Brand>RetireLabs</Navbar.Brand>
+          RetireLabs
         </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <MyLink href="/calculator">Calculator</MyLink>
-            <MyLink href="/about">About</MyLink>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      </div>
       <Component {...pageProps} />
     </Provider>
   );

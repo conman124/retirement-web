d6d0ad4ef54780be423bd11181eeb61ba6745ab9
Connor Hewitt <connor.hewitt@gmail.com>
2023-08-29 12:04:18 -0500

    Add icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,28 +6,33 @@ import "../styles/globals.css";
 import store from "../store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Navbar className="bg-primary shadow-lg">
-        <Navbar.Start className="hidden md:flex"></Navbar.Start>
-        <Navbar.Center>
-          <Link href="/">
-            <a className="btn text-primary-content btn-ghost normal-case text-xl">
-              RetireLabs
-            </a>
-          </Link>
-          <Link href="/calculator">
-            <a className="pl-4 pr-4 text-primary-content text-xl">Calculator</a>
-          </Link>
-          <Link href="/about">
-            <a className="pl-4 pr-4 text-primary-content text-xl">About</a>
-          </Link>
-        </Navbar.Center>
-        <Navbar.End className="hidden md:flex"></Navbar.End>
-      </Navbar>
-      <Component {...pageProps} />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Navbar className="bg-primary shadow-lg">
+                <Navbar.Start className="hidden md:flex"></Navbar.Start>
+                <Navbar.Center>
+                    <Link href="/">
+                        <a className="btn text-primary-content btn-ghost normal-case text-xl">
+                            <img src="icon.svg" className="h-full pr-3" />
+                            RetireLabs
+                        </a>
+                    </Link>
+                    <Link href="/calculator">
+                        <a className="pl-4 pr-4 text-primary-content text-xl">
+                            Calculator
+                        </a>
+                    </Link>
+                    <Link href="/about">
+                        <a className="pl-4 pr-4 text-primary-content text-xl">
+                            About
+                        </a>
+                    </Link>
+                </Navbar.Center>
+                <Navbar.End className="hidden md:flex"></Navbar.End>
+            </Navbar>
+            <Component {...pageProps} />
+        </Provider>
+    );
 }
 
 export default MyApp;
